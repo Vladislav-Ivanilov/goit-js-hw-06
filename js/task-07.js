1df079d0f3f9caa7e4961c3d84dfa1e3a1470fcb
Vladislav-Ivanilov <62110387+Vladislav-Ivanilov@users.noreply.github.com>
2022-12-15 17:56:35 +0200

    123

--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const inputSizeControl = document.getElementById("font-size-control");
-const spanText = document.getElementById("text");
-
-function handleInputRange(event) {
-  spanText.style.fontSize = event.currentTarget.value + "px";
-}
-
-inputSizeControl.addEventListener("input", handleInputRange);
